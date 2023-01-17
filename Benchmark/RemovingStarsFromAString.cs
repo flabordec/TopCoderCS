83478503079d60b6e85c4c9e3c636e1f3ef084f5
flabordec <flabordec@hotmail.com>
2023-01-17 15:17:55 -0600

    Adding one more problem.

---
+++ Benchmark/RemovingStarsFromAString.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Engines;
+using RemovingStarsFromAString;
+
+namespace Benchmark
+{
+    /*
+    | Method |     Mean |    Error |   StdDev |
+    |------- |---------:|---------:|---------:|
+    |  Array | 17.33 ns | 0.205 ns | 0.181 ns |
+    |   List | 75.62 ns | 0.385 ns | 0.342 ns |
+    */
+
+    [SimpleJob(RuntimeMoniker.Net60)]
+    public class BenchmarkRemovingStarsFromAString
+    {
+        // We cannot even test large numbers because the trash solution is so trash
+        public string s = "leet**cod*e";
+
+        [Benchmark]
+        public void Array() => new Solution().RemoveStars(s);
+
+        [Benchmark]
+        public void List() => new Solution().RemoveStarsList(s);
+    }
+}
---
+++ Challenges/RemovingStarsFromAString.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemovingStarsFromAString
+{
+    public class Solution
+    {
+        public string RemoveStars(string s)
+        {
+            var sOut = new char[s.Length];
+            int ix = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '*')
+                {
+                    if (ix > 0)
+                        ix--;
+                }
+                else
+                {
+                    sOut[ix] = s[i];
+                }
+            }
+            return new string(sOut, 0, ix);
+        }
+
+        public string RemoveStarsList(string s)
+        {
+            var sOut = new List<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '*')
+                {
+                    if (sOut.Any())
+                        sOut.RemoveAt(sOut.Count - 1);
+                }
+                else
+                {
+                    sOut.Add(s[i]);
+                }
+            }
+            return new string(sOut.ToArray());
+        }
+    }
+}
