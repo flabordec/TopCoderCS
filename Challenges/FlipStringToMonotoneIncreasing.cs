2974a371be42a773aef1a3ccc7779ad259ae67bf
flabordec <flabordec@hotmail.com>
2023-01-17 10:05:29 -0600

    New solutions

---
+++ Challenges/FlipStringToMonotoneIncreasing.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipStringToMonotoneIncreasing
+{
+    public class Solution
+    {
+        public int MinFlipsMonoIncr(string s)
+        {
+            bool counting = false;
+            int counter = 0;
+            int flips = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '1')
+                    counting = true;
+
+                if (counting)
+                {
+                    if (s[i] == '1')
+                        counter++;
+                    else
+                        flips++;
+
+                    flips = Math.Min(flips, counter);
+                }
+            }
+
+            return flips;
+        }
+    }
+}
---
+++ Challenges/NumberOfGoodWaysToSplitAString.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberOfGoodWaysToSplitAString
+{
+    public class Solution
+    {
+        public int NumSplits(string s)
+        {
+            Dictionary<char, int> left = new Dictionary<char, int>();
+            Dictionary<char, int> right = new Dictionary<char, int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!right.ContainsKey(s[i]))
+                    right.Add(s[i], 0);
+                right[s[i]]++;
+            }
+
+            int goodStrings = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!left.ContainsKey(s[i]))
+                    left.Add(s[i], 0);
+                left[s[i]]++;
+
+                right[s[i]]--;
+                if (right[s[i]] == 0)
+                    right.Remove(s[i]);
+
+                if (left.Count == right.Count)
+                {
+                    goodStrings++;
+                }
+            }
+
+            return goodStrings;
+        }
+    }
+}
