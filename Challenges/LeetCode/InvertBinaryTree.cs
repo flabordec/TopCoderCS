0a8ec2746a80904224f5a8e82f45042437b834e1
flabordec <flabordec@hotmail.com>
2023-02-18 16:12:13 -0600

    Couple more problems

---
+++ Challenges/LeetCode/InvertBinaryTree.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges.LeetCode.InvertBinaryTree
+{
+    // Definition for a binary tree node.
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class State
+    {
+        public TreeNode Current { get; }
+        public TreeNode Result { get; }
+
+        public State(TreeNode current, TreeNode result)
+        {
+            Current = current;
+            Result = result;
+        }
+    }
+
+    public class Solution
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+                return null;
+
+            var queue = new Queue<State>();
+            var result = new TreeNode(root.val);
+            queue.Enqueue(new State(root, result));
+            while (queue.Any())
+            {
+                var state = queue.Dequeue();
+
+                if (state.Current.right != null)
+                {
+                    state.Result.left = new TreeNode(state.Current.right.val);
+                    queue.Enqueue(new State(state.Current.right, state.Result.left));
+                }
+
+                if (state.Current.left != null)
+                {
+                    state.Result.right = new TreeNode(state.Current.left.val);
+                    queue.Enqueue(new State(state.Current.left, state.Result.right));
+                }
+
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Challenges/LeetCode/MinimumDistanceBetweenBstNodes.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges.LeetCode.MinimumDistanceBetweenBstNodes
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+    public class Solution
+    {
+        public int MinDiffInBST(TreeNode root)
+        {
+            var queue = new Queue<TreeNode>();
+            List<int> values = new List<int>();
+            values.Add(root.val);
+            queue.Enqueue(root);
+            while (queue.Any())
+            {
+                var curr = queue.Dequeue();
+
+                if (curr.right != null)
+                {
+                    values.Add(curr.right.val);
+                    queue.Enqueue(curr.right);
+                }
+
+                if (curr.left != null)
+                {
+                    values.Add(curr.left.val);
+                    queue.Enqueue(curr.left);
+                }
+
+            }
+
+            values.Sort();
+
+            int result = int.MaxValue;
+            int prev = values.First();
+            foreach (var value in values.Skip(1))
+            {
+                if (prev != value)
+                {
+                    result = Math.Min(result, Math.Abs(value - prev));
+                    prev = value;
+                }
+            }
+            return result;
+        }
+    }
+}
